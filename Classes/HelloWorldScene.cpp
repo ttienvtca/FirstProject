15beda4920b1e9c5bfccf2bca4135c1a40b9c3fe
ttienvtca <147244337+ttienvtca@users.noreply.github.com>
2023-10-27 23:05:40 +0700

    Commit

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -9,6 +9,7 @@ Scene* HelloWorld::createScene()
 {
 	return HelloWorld::create();
 }
+
 bool HelloWorld::init()
 {
 	if (!Scene::init())
@@ -25,9 +26,7 @@ bool HelloWorld::init()
 	this->addChild(_character, 1);
 	this->scheduleUpdate();
 
-
-
-	TMXTiledMap* map = TMXTiledMap::create("Maps/map1.tmx");
+	/*TMXTiledMap* map = TMXTiledMap::create("Maps/map1.tmx");
 
 	this->addChild(map);
 
@@ -39,7 +38,21 @@ bool HelloWorld::init()
 	position.y = charPoint["y"].asFloat();
 	_character->setPosition(position);
 
-	this->getDefaultCamera()->setPosition(position);
+	this->getDefaultCamera()->setPosition(position);*/
+
+
+	TMXTiledMap* map = TMXTiledMap::create("/Maps/Demo/map-demo.tmx");
+	this->addChild(map);
+
+	TMXObjectGroup* objGroup = map->getObjectGroup("SpawnPoint");
+	ValueMap charPoint = objGroup->getObject("CharacterSpawnPoint");
+
+	Vec2 position;
+	position.x = charPoint["x"].asFloat();
+	position.y = charPoint["y"].asFloat();
+
+	_character->setPosition(position);
+
 
 	return true;
 }
@@ -48,6 +61,9 @@ void HelloWorld::update(float dt)
 {
 	Vec2 direction = KeyboardInput::getInstance()->getDirection();
 	_character->setPosition(_character->getPosition() + direction * _moveSpeed * dt);
+
+	Camera* defaultCam = this->getDefaultCamera();
+	defaultCam->setPosition(_character->getPosition());
 }
 
 void HelloWorld::onEnter()
